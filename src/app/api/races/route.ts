6bcae3e450261ab9e733f3fa1ebdb537b59fd9ec
philosophynote <adverdest@gmail.com>
2024-12-25 07:26:25 +0900

    コンパイル失敗対応

--- src/app/api/races/route.ts
+++ src/app/api/races/route.ts
@@ -4,8 +4,8 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function GET() {
-  const now = new Date();
-  const nextDay = new Date(now.getTime() + 24 * 60 * 60 * 1000);
+  // const now = new Date();
+  // const nextDay = new Date(now.getTime() + 24 * 60 * 60 * 1000);
 
   const races = await prisma.race.findMany({
     // where: {
