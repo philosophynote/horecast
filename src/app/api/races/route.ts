ef7e4251efe51927606ec8dd995f46083898bad5
philosophynote <adverdest@gmail.com>
2024-12-25 01:25:11 +0900

    日付の指定を外す

--- src/app/api/races/route.ts
+++ src/app/api/races/route.ts
@@ -8,12 +8,12 @@ export async function GET() {
   const nextDay = new Date(now.getTime() + 24 * 60 * 60 * 1000);
 
   const races = await prisma.race.findMany({
-    where: {
-      race_time: {
-        gte: now,
-        lt: nextDay,
-      },
-    },
+    // where: {
+    //   race_time: {
+    //     gte: now,
+    //     lt: nextDay,
+    //   },
+    // },
     orderBy: { race_time: 'asc' },
   });
 
