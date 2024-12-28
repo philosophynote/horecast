2b423d148f1d6f9db8111edc9e30dddce782d393
philosophynote <adverdest@gmail.com>
2024-12-28 10:22:55 +0900

    日付を今日だけ

--- src/app/api/races/route.ts
+++ src/app/api/races/route.ts
@@ -4,16 +4,16 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function GET() {
-  // const now = new Date();
-  // const nextDay = new Date(now.getTime() + 24 * 60 * 60 * 1000);
+  const now = new Date();
+  const nextDay = new Date(now.getTime() + 24 * 60 * 60 * 1000);
 
   const races = await prisma.race.findMany({
-    // where: {
-    //   race_time: {
-    //     gte: now,
-    //     lt: nextDay,
-    //   },
-    // },
+    where: {
+      race_time: {
+        gte: now,
+        lt: nextDay,
+      },
+    },
     orderBy: { race_time: 'asc' },
   });
 
