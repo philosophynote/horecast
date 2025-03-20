b52c785790c5ef10d564eaf3db780089cf506d57
nao <78991083+philosophynote@users.noreply.github.com>
2025-03-20 23:23:55 +0900

    データがあるもののみ表示 (#9)

--- src/app/api/races/route.ts
+++ src/app/api/races/route.ts
@@ -4,16 +4,14 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function GET() {
-  // const now = new Date(Date.now());
-  // const nextDay = new Date(now.getTime() + 24 * 60 * 60 * 1000);
+  const targetDate = new Date('2025-03-16T00:00:00+09:00');
 
   const races = await prisma.race.findMany({
-    // where: {
-    //   race_time: {
-    //     gte: now,
-    //     lt: nextDay,
-    //   },
-    // },
+    where: {
+      race_time: {
+        gte: targetDate,
+      },
+    },
     orderBy: { race_time: 'asc' },
   });
 
